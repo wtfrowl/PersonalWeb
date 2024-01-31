2e881774c55cec711c59b3909f94f9dc0c5a359a
wtfrowl <imrhjat@rediffmail.com>
2024-02-01 04:38:32 +0530

    Margin Fixed, backdrop added in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import { BiSolidMusic } from "react-icons/bi";
 import { NavLink } from 'react-router-dom';
 const Header = ()=>{
     return(
-    <div className='sticky top-0 z-50 '>
+    <div className='sticky top-0 z-60 backdrop-blur-sm'>
 
        <header className="flex bg-green-800/60 h-20 justify-between md:justify-evenly items-center ">
-      <div className="ml-5 md:m-0"><NavLink className={({isActive})=> `${isActive?" mx-5 bg-red-800":"m-5"}`}
+      <div className="ml-5 md:my-5"><NavLink className={({isActive})=> `${isActive?"my-5":"my-5"}`}
     to="/"><img src={wtfrowl} /></NavLink></div>
       <div className="mr-5 md:m-0">
         <NavLink className={({isActive})=> `${isActive?"text-green-900":""}`}
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,9 +6,9 @@ const Nav = () => {
 
 return(
 
-<nav className='fixed bottom-4 md:bottom-20 w-full overflow-hidden'>
+<nav className='fixed bottom-4 md:bottom-16 w-full overflow-hidden z-20'>
 <div className="container mx-auto flex justify-center">
-    <div className="w-full bg-green-600/60 border h-[90px] backdrop-blur-3xl rounded-full max-w-[400px] flex justify-evenly items-center mx-5 lg:mx-0">
+    <div className="w-full bg-green-600/60 border h-[90px] backdrop-blur-md rounded-full max-w-[400px] flex justify-evenly items-center mx-4 lg:mx-0">
         <NavLink to="">
         <BiHomeAlt className='curoser-pointer w-[45px] h-[45px] p-1' />
         </NavLink>
